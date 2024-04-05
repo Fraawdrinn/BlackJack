04f86cff3223ad28554bef298ab0fabe7629d846
Fraawdrinn <chatoui.wael40@gmail.com>
2024-04-05 18:35:51 +0200

    added init and assets

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import pygame as pg
+from random import randint
+import time
 
-pg.display()
+# Initialisation
+playerHand = dealerHand = []
+cards = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", 
+         "A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
+         "A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
+         "A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+# Distribuer les cartes
+def distrib(Hand):
+    """Distribution de 2 cartes"""
+    for _ in range(2):
+        Hand.append(randint(len(cards)))
+
+# Calculer le total
+        
+# Vérifier le gagnant
+        
+# Boucle de jeu
+    
