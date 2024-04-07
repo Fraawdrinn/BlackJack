f6e5517a73470f06cfd26ddf24972d3ce3aad239
Fraawdrinn <chatoui.wael40@gmail.com>
2024-04-07 05:52:51 +0200

    started card animation

--- main.py
+++ main.py
@@ -12,33 +12,59 @@ cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Jack", "Queen", "Ki
 
 pg.init()
 pg.display.set_caption("BlackJack Game")
+clock = pg.time.Clock()
 window_width = 800
 window_height = 600
 
-background = pg.image.load('assets/bg.png') # Chargement de l'image
-background = pg.transform.scale(background, (window_width, window_height)) # Taille de l'image
+background = pg.image.load('assets/bg.png')
+background = pg.transform.scale(background, (window_width, window_height))
 
-gameDisplay = pg.display.set_mode((window_width, window_height)) # Taille de la page
-gameDisplay.blit(background, (0, 0)) # Affiche l'image de fond
+gameDisplay = pg.display.set_mode((window_width, window_height))
+gameDisplay.blit(background, (0, 0))
 
-    # Chargement d'images
 pioche = pg.image.load('assets/cartes/card-extras/card_back.png')
 pioche = pg.transform.scale(pioche, (200, 200))
 gameDisplay.blit(pioche, (-20, 200))
 
+pioche2 = pg.image.load('assets/cartes/card-extras/card_back.png')
+pioche2 = pg.transform.scale(pioche2, (200, 200))
 
+# Distribuer les cartes
+def distrib(player):
+    """Fonction permettant de piocher une carte"""
+    card = random.choice(cards)
+    cards.remove(card)
+    player.append(card)
 
-# Calculer le total
+def AnimPioche():
+    """Animation de pioche (vers le premier slot de carte)"""
+    pioche2_x = -20  # Position initiale en x de pioche2
+    pioche2_y = 200  # Position initiale en y de pioche2
 
+    while pioche2_x < 340 and pioche2_y < 400:
+        # Animation de pioche2
+        pioche2_x += 2  # Déplacement horizontal
+        pioche2_y += 2
 
-# Vérifier le gagnant
+        gameDisplay.blit(background, (0, 0))  # Afficher le fond à chaque boucle pour effacer les anciens objets
+        gameDisplay.blit(pioche, (-20, 200))  # Afficher pioche
+        gameDisplay.blit(pioche2, (pioche2_x, pioche2_y))  # Afficher pioche2 à sa nouvelle position
+
+        pg.display.update()  # Mettre à jour l'écran une fois par boucle
 
 # Boucle de jeu
-
 while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-    pg.display.update()
+
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                AnimPioche()
+    
+    pg.display.update()  # Mettre à jour l'écran une fois par boucle
+
+    clock.tick(60)
+
 pg.quit()
 quit()
